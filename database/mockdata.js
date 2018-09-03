8084260eff9c6368143e45c868a72d0693454f0c
Ethan Ng <ethanng329@gmail.com>
2018-09-03 18:03:47 +0100

    mock database created

---
+++ database/mockdata.js
@@ -0,0 +1,39 @@
+const mockdata = {
+  '1': {
+    id: '1',
+    company_name: 'Dog Ltd',
+    product_name: 'Dog bone',
+    price: 3,
+    description: 'tasty bone for all dogs'
+  },
+  '2': {
+    id: '2',
+    company_name: 'Dog Ltd',
+    product_name: 'Dog biscult',
+    price: 5,
+    description: 'cruchy treats'
+  },
+  '3': {
+    id: '3',
+    company_name: 'Cat Ltd',
+    product_name: 'Catnip',
+    price: 10,
+    description: 'loved by all cats'
+  },
+  '4': {
+    id: '4',
+    company_name: 'Rabbit Ltd',
+    product_name: 'organic carrots',
+    price: 100,
+    description: 'chemical free, wonky shapes'
+  },
+  '5': {
+    id: '5',
+    company_name: 'Cat Ltd',
+    product_name: 'soft toys',
+    price: 5,
+    description: 'purRfect for indoor games'
+  }
+};
+
+module.exports = mockdata;
