cdff11b5b30b2654f5e2fe1150bd0ed48752dd26
Ethan Ng <ethanng329@gmail.com>
2018-09-04 12:36:46 +0100

    shopping basket set up

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from './components/Header';
-// import Selector from './components/Selector';
 import { isArray } from 'util';
 import SearchResults from './components/SearchResults';
 
@@ -10,10 +9,13 @@ class App extends React.Component {
 
     this.state = {
       company: [],
-      product: []
+      product: [],
+      currentOrder: {}
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleClickAdd = this.handleClickAdd.bind(this);
+    this.handleClickMinus = this.handleClickMinus.bind(this);
   }
 
   componentDidMount() {
@@ -26,6 +28,7 @@ class App extends React.Component {
         });
       })
       .catch(error => console.log(error));
+    console.log(this.state.currentOrder)
   }
 
   handleChange(event) {
@@ -39,13 +42,38 @@ class App extends React.Component {
       });
   }
 
+  handleClickAdd(event) {
+    const productname = event.target.name;
+    const quantity = this.state.currentOrder[productname] ? this.state.currentOrder[productname] : 0;
+    const newOrder = { [productname]: quantity + 1 };
+    const updatedOrder = Object.assign({}, this.state.currentOrder, newOrder);
+    this.setState({
+      currentOrder: updatedOrder
+    })
+    console.log(this.state.currentOrder)
+  }
+
+  handleClickMinus(event) {
+    const productname = event.target.name;
+    const quantity = this.state.currentOrder[productname];
+    if (this.state.currentOrder[productname] > 1) {
+      const newOrder = { [productname]: quantity - 1 };
+      const updatedOrder = Object.assign({}, this.state.currentOrder, newOrder);
+      this.setState({ currentOrder: updatedOrder })
+    } else if ((this.state.currentOrder[productname] = 1)) {
+      const updatedOrder = Object.assign({}, this.state.currentOrder);
+      delete updatedOrder[productname];
+      this.setState({ currentOrder: updatedOrder })
+    }
+
+  }
   render() {
-    console.log(this.state.product);
+    console.log(this.state.currentOrder);
     return (
       <div>
         <Header title="Company price list" />
         <label>Company</label>
-        <select onChange={this.handleChange}>
+        <select defaultValue="Select Company here" onChange={this.handleChange}>
           <option disabled>Select Company here</option>
           {this.state.company.map(option => (
             <option default="" key={option} value={option}>
@@ -64,7 +92,21 @@ class App extends React.Component {
             </option>
           ))}
         </select>
-        <SearchResults result={this.state.product} />
+        <SearchResults onClick={this.handleClickAdd} result={this.state.product} onClickMinus={this.handleClickMinus} />
+
+        <div>
+          Shopping Basket:
+
+          {this.state.currentOrder ? Object.keys(this.state.currentOrder).map(item => {
+            const itemQuantity = this.state.currentOrder[item];
+            const itemName = item;
+            return (<div key={itemName}>  <p>{item} quantity:{itemQuantity}</p>
+            </div>)
+          }) : ""}
+
+          <button>Submit Order</button>
+        </div>
+
       </div>
     );
   }
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -3,9 +3,21 @@ import React from 'react';
 class SearchResults extends React.Component {
   constructor(props) {
     super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClickMinus = this.handleClickMinus.bind(this);
+  }
+
+  handleClick(event) {
+    this.props.onClick(event);
+
+  }
+
+  handleClickMinus(event) {
+    this.props.onClickMinus(event);
   }
   render() {
-    console.log(this.props.result);
+
     return (
       <div>
         <p>search results</p>
@@ -15,17 +27,21 @@ class SearchResults extends React.Component {
               <th>Company</th>
               <th>Product</th>
               <th>Price</th>
+              <th>Description</th>
+              <th>Buy</th>
             </tr>
             {this.props.result
               ? this.props.result.map(data => {
-                  return (
-                    <tr key={data.id}>
-                      <td>{data.company_name}</td>
-                      <td>{data.product_name}</td>
-                      <td>{data.price}</td>
-                    </tr>
-                  );
-                })
+                return (
+                  <tr key={data.id}>
+                    <td>{data.company_name}</td>
+                    <td>{data.product_name}</td>
+                    <td>{data.price}</td>
+                    <td>{data.description}</td>
+                    <td><button name={data.product_name} onClick={this.handleClick}>+</button><button name={data.product_name} onClick={this.handleClickMinus}>-</button></td>
+                  </tr>
+                );
+              })
               : ''}
           </tbody>
         </table>
